1db96f98571e4030df883917eac40cc7045f8662
Rui Franscisco de Paula Inácio Diniz <rui.pdiniz@gmail.com>
2026-05-20 13:10:19 -0300

    feat(content): exporta conteudo versionado

---
+++ src/content/index.ts
@@ -0,0 +1,13 @@
+export { companyContent } from './company';
+export { ctaContent } from './ctas';
+export { equipmentContent } from './equipment';
+export { homeContent } from './home';
+export { navigationItems } from './navigation';
+export { servicesContent } from './services';
+export {
+  findDuplicateSlugs,
+  findEmptyTextFields,
+  hasRequiredTextFields,
+  hasUniqueSlugs,
+} from './validators';
+export type { CompanyContent, ContentItem, CTAContent, HomeContent, NavigationItem } from './types';
