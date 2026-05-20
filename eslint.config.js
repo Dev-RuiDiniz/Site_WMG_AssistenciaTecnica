49bd42b87703c9a091d323426123c07e974f5660
Rui Franscisco de Paula Inácio Diniz <rui.pdiniz@gmail.com>
2026-05-20 12:14:19 -0300

    build(lint): corrige configuracao flat do eslint

--- eslint.config.js
+++ eslint.config.js
@@ -2,19 +2,17 @@ import js from '@eslint/js';
 import globals from 'globals';
 import reactHooks from 'eslint-plugin-react-hooks';
 import reactRefresh from 'eslint-plugin-react-refresh';
+import tseslint from 'typescript-eslint';
 
-export default [
+export default tseslint.config(
   { ignores: ['dist'] },
+  js.configs.recommended,
+  ...tseslint.configs.recommended,
   {
     files: ['**/*.{ts,tsx}'],
-    extends: [js.configs.recommended],
     languageOptions: {
       ecmaVersion: 2022,
       globals: globals.browser,
-      parserOptions: {
-        ecmaVersion: 'latest',
-        sourceType: 'module',
-      },
     },
     plugins: {
       'react-hooks': reactHooks,
@@ -25,4 +23,4 @@ export default [
       'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
     },
   },
-];
+);
