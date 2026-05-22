625bddb985830e5756e7fdbf773a51c63a397a9d
Rui Franscisco de Paula Inácio Diniz <rui.pdiniz@gmail.com>
2026-05-22 08:30:02 -0300

    feat(analytics): inicializa analytics na aplicação

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,11 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
+import { initializeAnalytics } from './analytics/analytics';
 import App from './App';
 import './styles/global.css';
+
+initializeAnalytics();
 
 createRoot(document.getElementById('root') as HTMLElement).render(
   <StrictMode>
